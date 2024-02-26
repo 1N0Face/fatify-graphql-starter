3c42a1c7c6095a3cb839b18b9448f2a04631c6d5
1N0Face <maxim.black2004@gmail.com>
2024-02-26 19:54:28 +0200

    Created nestJS starter project

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { Logger } from 'nestjs-pino';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter(), { bufferLogs: true });
+  app.useLogger(app.get(Logger));
+  await app.listen(3000, '0.0.0.0');
+}
+
+bootstrap();
